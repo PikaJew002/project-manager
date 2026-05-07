046c96d95678b58b81b672445103a700a308fe2b
Aaron Eisenberg <aaron@ironmthome.com>
2026-05-07 16:51:37 -0400

    Add favicon files and update app layout to include them
    - Added a new SVG favicon for the project.
    - Updated the app layout to link both the ICO and SVG favicon files.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <title inertia>{{ config('app.name', 'Project Manager') }}</title>
         @routes
         @vite('resources/js/app.js')
