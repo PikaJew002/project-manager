3dd58bf6d15215e9c2a169813fa644f4b774f701
Aaron Eisenberg <aaron@ironmthome.com>
2026-05-08 10:51:20 -0400

    Add Sentry Vite plugin for enhanced error tracking
    - Integrated the Sentry Vite plugin into the Vite configuration for improved error monitoring.
    - Updated package.json and package-lock.json to include the new Sentry Vite plugin dependency.
    - Configured Sentry with authentication token and project details from environment variables.

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,33 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    build: {
+      sourcemap: true,
+    },
     plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+      laravel({
+        input: 'resources/js/app.js',
+        refresh: true,
+      }),
+      vue({
+        template: {
+          transformAssetUrls: {
+            base: null,
+            includeAbsolute: false,
+          },
+        },
+      }),
+      sentryVitePlugin({
+        authToken: env.SENTRY_AUTH_TOKEN,
+        org: 'aaron-eisenberg',
+        project: 'project-manager-vue',
+      }),
     ],
+  };
 });
