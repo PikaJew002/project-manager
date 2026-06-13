50c06c63d28a842361a551df19b72e60f4b34462
Aaron Eisenberg <aaron@ironmthome.com>
2026-06-13 00:10:53 -0400

    Add one off email functionality
    - Created a new Mailable class `ShabbatZoomLink` to handle the sending of Zoom meeting invitations for Shabbat services.
    - Implemented email content and envelope with dynamic date handling.
    - Scheduled weekly and monthly email dispatches in `bootstrap/app.php` to send the Zoom link to specified recipients.
    - Added a Blade view for the email content to format the invitation details.

---
+++ resources/views/email/zoom-link.blade.php
@@ -0,0 +1,15 @@
+Baruch Haba is inviting you to a scheduled Zoom meeting.
+
+Topic: Shabbat Service Baruch Haba
+Time: {{ $dateString }} 11:00 AM
+
+Join Zoom Meeting
+{{ $link }}
+
+Meeting ID: {{ $meetingId }}
+Passcode: {{ $meetingPassword }}
+
+---
+
+One tap mobile
+{{ $mobileLink }}
